7cd1a4ba88c57305b21592906d1e48dd55d2d661
marcioc0sta <marciocst2@gmail.com>
2014-12-18 16:34:25 -0200

    Update team3 code

---
+++ time3/game.rb
@@ -0,0 +1,103 @@
+# encoding: UTF-8
+
+require 'io/console'
+
+def read_char
+  STDIN.echo = false
+  STDIN.raw!
+
+  input = STDIN.getc.chr
+  if input == "\e" then
+    input << STDIN.read_nonblock(3) rescue nil
+    input << STDIN.read_nonblock(2) rescue nil
+  end
+ensure
+  STDIN.echo = true
+  STDIN.cooked!
+
+  return input
+end
+
+def show_single_key(read_char)
+  c = read_char
+
+  case c
+  when "\e[C"
+    return :right
+  when "\e[D"
+    return :left
+  end
+end
+
+def process_scenario
+  elem = $map.pop
+  $map.unshift elem
+end
+
+def screen()
+  text = "\e[H\e[2J\n"
+  text += "Score: #{$score}\n"
+  car_area = "|                    |".split("")
+  car_area[$col] = "A"
+  text += $map.join("\n")
+  text += "\n" + car_area.join("")
+  puts text
+end
+
+def change_car_position
+  $key = show_single_key(read_char)
+  if $key == :right
+    $col += 1
+  elsif $key == :left
+    $col -= 1
+  end
+end
+
+$map = [
+  "|                    |",
+  "|                    |",
+  "|                    |",
+  "|                    |",
+  "||                  ||",
+  "|                    |",
+  "|                    |",
+  "|                    |",
+  "|                    |",
+  "||                  ||",
+  "|                    |",
+  "|                    |",
+  "|                    |",
+  "|                    |",
+  "|                    |",
+  "||                  ||",
+  "|                    |",
+  "|                    |",
+  "|                    |",
+  "||                  ||",
+  "|                    |",
+  "|                    |",
+  "|                    |",
+  "|                    |",
+  "||                  ||",
+  "|                    |",
+  "|                    |",
+  "|                    |",
+  "||                  ||",
+  "|                    |",
+  "|                    |",
+]
+
+$key = nil
+$col = 12
+$score = 0
+
+def start
+  loop do
+    screen()
+    # change_car_position
+    process_scenario
+    $key = nil
+    $score += 10
+    sleep 0.3
+  end
+end
---
+++ time3/game_test.rb
@@ -0,0 +1,16 @@
+require './game'
+require 'minitest/autorun'
+
+describe "test" do
+
+  describe "show_single_key" do
+    it "Work with right" do
+      show_single_key("\e[C").must_equal :right
+    end
+
+    it "Work with left" do
+      show_single_key("\e[D").must_equal :left
+    end
+  end
+
+end
