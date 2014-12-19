c6ea492fbfb3bcf8b909729ff64ac74a9267daa2
Marcio Junior <marciojunior_eu@yahoo.com.br>
2014-12-19 10:25:16 -0200

    Updated main

--- road_raid.rb
+++ road_raid.rb
@@ -1,3 +1,53 @@
-require "./time2/game"
-require "./time2/main"
-Main.new.call
+# encoding: UTF-8
+
+require "./game"
+require 'timeout'
+
+def start
+  @game = Game.new
+  loop do
+    process_frame
+  end
+end
+
+def update_car_position
+  char = read_with_timeout
+  case char
+  when "d"
+    @game.move_right
+  when "a"
+    @game.move_left
+  end
+end
+
+def read_with_timeout
+  Timeout::timeout(0.5) do
+    input = read_char
+    if input == 'q'
+      exit
+    end
+    return input
+  end
+rescue Timeout::Error
+  process_frame
+end
+
+def read_char
+  begin
+    system("stty raw -echo")
+    str = STDIN.getc
+  ensure
+    system("stty -raw echo")
+  end
+  str.chr
+end
+
+def process_frame
+  @game.pass
+  puts @game.screen
+  @game.score += 10
+  update_car_position
+  sleep 0.3
+end
+
+start
