743d393006dbe46e2b0aa45a9c330ad103bcc49e
Marcio Junior <marciojunior_eu@yahoo.com.br>
2014-12-19 09:25:21 -0200

    Merge code

--- time1/cli.rb
+++ time1/cli.rb
@@ -1,23 +1,33 @@
 # encoding: UTF-8
 
 require "./game"
+require 'timeout'
 
 class CLI
 
   class << self
 
     def start
-      game = Game.new
+      @game = Game.new
       loop do
-        puts game.render
-        game.score += 10
-        update_car_position(game)
-        sleep 0.3
+        process_frame
       end
     end
 
-    def update_car_position(game)
-      game.move_car_to(direction_for_key(read_char))
+    def update_car_position
+      @game.move_car_to(direction_for_key(read_with_timeout))
+    end
+
+    def read_with_timeout
+      Timeout::timeout(0.5) do
+        input = read_char
+        if input == 'q'
+          exit
+        end
+        return input
+      end
+      rescue Timeout::Error
+        process_frame
     end
 
     def read_char
@@ -30,15 +40,20 @@ class CLI
       str.chr
     end
 
-    def direction_for_key(read_char) 
-      c = read_char
-
-      case c
+    def direction_for_key(char) 
+      case char
       when "d"
         return :right
       when "a"
         return :left
       end
+    end
+
+    def process_frame
+      puts @game.render
+      @game.score += 10
+      update_car_position
+      sleep 0.3
     end
 
   end
