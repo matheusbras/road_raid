1544d4cd142f965c8f3752f01131efc806874e19
Mauro George <maurogot@gmail.com>
2014-12-19 12:26:20 -0200

    Lil refact

--- game.rb
+++ game.rb
@@ -3,6 +3,10 @@ require 'io/console'
 require 'timeout'
 
 class Game
+  SCREEN_START = 0
+  SCREEN_END = 4
+  MOVEMENT_SPEED = 1
+  CAR = 'A'
 
   MAP =["     ",
         "     ",
@@ -10,26 +14,26 @@ class Game
         "     "]
 
   def initialize
-    @car = [' ', ' ', 'A', ' ', ' ']
+    @car = [' ', ' ', CAR, ' ', ' ']
     @car_position = 2
     @screen = MAP
   end
 
   def screen
-    @screen[4] = @car.join
+    @screen[SCREEN_END] = @car.join
     @screen.join("\n")
   end
 
   def move_left
     @car[@car_position] = ' '
-    @car_position -= 1
-    @car[@car_position] = 'A'
+    @car_position -= MOVEMENT_SPEED
+    @car[@car_position] = CAR
   end
 
   def move_right
     @car[@car_position] = ' '
-    @car_position += 1 if @car_position < 4
-    @car[@car_position] = 'A'
+    @car_position += MOVEMENT_SPEED if @car_position < SCREEN_END
+    @car[@car_position] = CAR
   end
 end
 
