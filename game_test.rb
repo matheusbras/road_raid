c29cd4f0fff84dc68edfa8e087c716b32ca822d4
Anezio Campos <newdevas@gmail.com>
2014-12-19 10:18:52 -0200

    bla

--- game_test.rb
+++ game_test.rb
@@ -12,4 +12,23 @@ describe "The game works" do
                             "     ",
                             "  A  "].join("\n") 
   end
+  
+  it "move the car" do
+    game = Game.new
+    game.move_left
+    
+    game.screen.must_equal ["     ",
+                            "     ",
+                            "     ",
+                            "     ",
+                            " A   "].join("\n")
+                            
+    game.move_right
+
+    game.screen.must_equal ["     ",
+                            "     ",
+                            "     ",
+                            "     ",
+                            "  A  "].join("\n") 
+  end
 end
